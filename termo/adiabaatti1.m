3afc46fd129d4b6ebfd086d667b2779cbf99230c
Oskari Mantere <osku.mantere@gmail.com>
2016-12-07 22:18:21 +0200

    Ekan kerran mittaukset

--- termo/adiabaatti1.m
+++ termo/adiabaatti1.m
@@ -1,13 +1,13 @@
 load carsmall
 
 p1 = 102.9;
-h1 = [98 78 60 51 97 82 64 49 37 70];
-h2 = [63 45 44 26 64 44 41 31 12 40];
-p2 = [163.9 186.2 142.4 183.1 159.3 199.5 162.2 158.9 224.6 182.2];
+h1 = [90 66 35 90 65 90 62 88 65 34];
+h2 = [66 35 19 65 35 33 17 65 33 15];
+p2 = [133 166 142 134 163 205 211 132 165 152];
 
-Vp = (4/2)^2*pi*(2.5 + 8 + 10);
+Vp = 500*2^2*pi;
 
-V = @(h) Vp + h*8^2*pi;
+V = @(h) Vp + h*8.25^2*pi;
 vakio = @(p1_, p2_, V1_, V2_) log(p2_/p1_)/log(V1_/V2_);
 
 vakiot = [];
@@ -21,7 +21,11 @@ end
 
 vakiot
 scatter(p2, vakiot)
+xlabel('Loppupaine')
+ylabel('Adiabaattivakio')
 
+figure
+scatter(p2, vakiot)
 hold on
 lm = fitlm(p2, vakiot, 'linear')
 plot(lm)
@@ -29,3 +33,5 @@ plot(lm)
 xlabel('Loppupaine')
 ylabel('Adiabaattivakio')
 title('')
+
+
---
+++ termo/adiabaatti2.m
@@ -0,0 +1,37 @@
+load carsmall
+
+p1 = 102.9;
+h1 = [98 78 60 51 97 82 64 49 37 70];
+h2 = [63 45 44 26 64 44 41 31 12 40];
+p2 = [163.9 186.2 142.4 183.1 159.3 199.5 162.2 158.9 224.6 182.2];
+
+Vp = 500*2^2*pi;
+
+V = @(h) Vp + h*8.25^2*pi;
+vakio = @(p1_, p2_, V1_, V2_) log(p2_/p1_)/log(V1_/V2_);
+
+vakiot = [];
+
+for i=1:length(p2)
+    V1 = V(h1(i));
+    V2 = V(h2(i));
+    gamma = vakio(p1, p2(i), V1, V2);
+    vakiot = [vakiot gamma];
+end
+
+vakiot
+scatter(p2, vakiot)
+xlabel('Loppupaine')
+ylabel('Adiabaattivakio')
+
+figure
+scatter(p2, vakiot)
+hold on
+lm = fitlm(p2, vakiot, 'linear')
+plot(lm)
+
+xlabel('Loppupaine')
+ylabel('Adiabaattivakio')
+title('')
+
+
